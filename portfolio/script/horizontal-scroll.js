79a854756011a7cc135c73bd9ed53594d60b8086
Jimmy <jimmychu82@gmail.com>
2020-08-04 21:22:05 -0700

    apply modal image content

--- portfolio/script/horizontal-scroll.js
+++ portfolio/script/horizontal-scroll.js
@@ -74,3 +74,28 @@ $(function() {
     updateUI();
   });
 });
+
+
+
+// modal image
+
+
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+
+function modalClick(n) {
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var imgId = "modal" + n;
+  var img = document.getElementById(imgId);
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
