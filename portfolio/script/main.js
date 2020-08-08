aad206be9b25669e52e294379dae8917700782d1
Jimmy <jimmychu82@gmail.com>
2020-08-08 16:21:56 -0700

    small visual imporvemnt on main nav and index page

--- portfolio/script/main.js
+++ portfolio/script/main.js
@@ -1,11 +1,15 @@
+var offset = -100;
+
 $(document).ready(function() {
+
+  navChange();
+
   $("#back2Top").click(function(event) {
     event.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
   });
 
-  var offset = -100;
   $("#product-link").click(function(event) {
     event.preventDefault();
     $("html, body").animate({
@@ -33,13 +37,40 @@ $(document).ready(function() {
 });
 
 
+
 $(window).scroll(function() {
   var height = $(window).scrollTop();
-  if (height > 100) {
+  if (height > 350) {
     $('#back2Top').fadeIn();
     $('#hint').fadeOut();
   } else {
     $('#back2Top').fadeOut();
     $('#hint').fadeIn();
   }
+
+
+
+  navChange();
+
 });
+
+function navChange() {
+  var secondOffset = -300;
+
+  var about = $("#about").offset().top;
+  var product = $("#project").offset().top;
+
+  if ($(window).scrollTop() >= (about + offset + secondOffset)) {
+    console.log(1);
+    $('#about-link').addClass('visit');
+    $('#product-link').removeClass('visit');
+  } else if ($(window).scrollTop() >= (product + offset + secondOffset)) {
+    console.log(2);
+    $('#about-link').removeClass('visit');
+    $('#product-link').addClass('visit');
+  } else {
+    console.log(3);
+    $('#about-link').removeClass('visit');
+    $('#product-link').removeClass('visit');
+  }
+}
